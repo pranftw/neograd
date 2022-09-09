aeaf450594b4ed659c5c8c8252f5f0dbffada3e6
Pranav Sastry <pranava.sri@gmail.com>
2022-09-09 12:59:00 +0530

    Trying to solve the memory issue, i think ive gotten it right this time

--- tests/test.py
+++ tests/test.py
@@ -1,45 +1,193 @@
 import _setup
-import neograd as ng
+from neograd.autograd.utils import process_data, mul_shape_dims, unflatten_data
+import weakref
 import numpy as np
-from neograd.nn.loss_fns import BinaryCrossEntropy
-from neograd.nn.optim import GD
-from neograd.nn.utils import get_batches
-from sklearn.datasets import make_circles
-from sklearn.model_selection import train_test_split
 
-X, y = make_circles(n_samples=1000, noise=0.05, random_state=100)
-X_train, X_test, y_train, y_test = train_test_split(X,y)
 
-X_train, X_test = ng.tensor(X_train.T), ng.tensor(X_test.T)
-y_train, y_test = ng.tensor(y_train.T.reshape(1,750)), ng.tensor(y_test.T.reshape(1,250))
+"""
+  When operation occurs add operands, set backward_fn
+"""
 
-num_train = 750
-num_test = 250
-num_iter = 1
-
-class NN(ng.nn.Model):
+class Node:
   def __init__(self):
-    super().__init__(self)
-    self.stack = ng.nn.Sequential(
-      ng.nn.Linear(2,100),
-      ng.nn.ReLU(),
-      ng.nn.Linear(100,1),
-      ng.nn.Sigmoid()
-    )
+    self.operands = [] # can this be weakref
+    self.children = []
+    self.needs_broadcasting = True
+    self.visited = False
   
-  def forward(self, inputs):
-    return self.stack(inputs)
+  def top_sort(self):
+    sorted_tensors = []
+    if self.are_children_visited(): # All children are resolved
+      self.visited = True
+      sorted_tensors.append(self)
+      for operand in self.operands:
+        if not(operand.visited):
+          sorted_tensors+=operand.top_sort()
+    else:
+      for child in self.children: # Resolve children first
+        if not(child.visited):
+          sorted_tensors+=child.top_sort()
+      self.visited = False
+      sorted_tensors.append(self)
+    return sorted_tensors
+  
+  def add_child(self, child):
+    self.children.append(child)
+  
+  def add_operand(self, operand):
+    self.operands.append(operand)
+  
+  def node_backward(self):
+    self.reset_visited()
+    self.visit_all_children()
+    sorted_tensors = self.top_sort()
+    self.reset_visited()
+    self.visit_all_children()
+    for tens in sorted_tensors:
+      tens.visited = True
+      upper_grad = tens.grad
+      tens._backward(upper_grad)
+  
+  def _backward(self, upper_grad):
+    if len(self.operands)!=0:
+      if self.needs_broadcasting:
+        upper_grad = upper_grad.flatten()
+      self.backward_fn()
+      for operand in self.operands:
+        if operand.requires_grad and operand.are_children_visited():
+          operand.visited = True
+          grad = operand.grad_fn(upper_grad)
+          grad = unflatten_data(grad, operand.shape, self.shape)
+          grad = grad.reshape(operand.shape)
+          operand.grad+=grad
+  
+  def visit_all_children(self):
+    for child in self.children:
+      child.visited = True
+  
+  def are_children_visited(self):
+    for child in self.children:
+      if not(child.visited):
+        return False
+    return True
 
-model = NN()
-loss_fn = BinaryCrossEntropy()
-optim = GD(model.get_params(), 0.05)
+  def reset_visited(self):
+    self.visited = False
+    for operand in self.operands:
+      operand.visited = False 
 
-# train_data_gen = get_batches(X_train, y_train, num_train, 50) TODO: ISSUE WRT DIMS
 
-for iter in range(num_iter):
-  optim.zero_grad()
-  outputs = model(X_train)
-  loss = loss_fn(outputs, y_train)
-  loss.backward()
-  optim.step()
-  print(f"iter {iter+1}/{num_iter}\nloss: {loss}\n")
+class Tensor(Node):
+  def __init__(self, data, requires_grad=False):
+    super().__init__()
+    self.data = data
+    self.requires_grad = requires_grad
+    self.grad = 0. if requires_grad else None
+    self.grad_fn = None
+    self.backward_fn = None
+  
+  def backward(self, upper_grad=1.0):
+    upper_grad = process_data(upper_grad)
+    self.grad+=upper_grad
+    self.node_backward()
+  
+  def zero_grad(self):
+    if self.grad:
+      self.grad = 0.
+  
+  def set_grad_fn(self, grad_fn):
+    if self.requires_grad:
+      self.grad_fn = grad_fn
+    else:
+      self.grad_fn = None
+  
+  def __add__(self, other):
+    return Add(self, other).forward()
+  
+  def __radd__(self, other):
+    return Add(other, self).forward()
+  
+  @property
+  def shape(self):
+    return self.data.shape
+  
+  @property
+  def data(self):
+    return self._data
+  
+  @data.setter
+  def data(self, data):
+    self._data = process_data(data)
+
+
+class Operation:
+  def __init__(self, operation, needs_broadcasting, *operands):
+    tensors = self.process_operands(operands)
+    self.tensors = [weakref.proxy(tens) for tens in tensors]
+    self.operation = weakref.proxy(operation)
+    self.needs_broadcasting = needs_broadcasting
+
+  def process_operands(self, operands):
+    operands = list(operands)
+    for i,operand in enumerate(operands):
+      if not isinstance(operand, Tensor):
+        operands[i] = Tensor(operand)
+    return tuple(operands)
+  
+  def get_tensors(self):
+    if len(self.tensors)==0:
+      return None
+    elif len(self.tensors)==1:
+      return self.tensors[0]
+    else:
+      return self.tensors
+  
+  def get_broadcast_shape(self):
+    if self.needs_broadcasting:
+      try:
+        return np.broadcast_shapes(*(tens.data.shape for tens in self.tensors))
+      except ValueError:
+        return None
+    else:
+      return None
+  
+  def check_result_requires_grad(self):
+    for tens in self.tensors:
+      if tens.requires_grad:
+        return True
+    return False
+  
+  def add_edges(self, result):
+    for operand in self.tensors:
+      operand.add_child(result)
+      result.add_operand(operand)
+  
+  def get_result_tensor(self, result):
+    result = result.astype(np.ndarray)
+    result = Tensor(result, requires_grad=self.check_result_requires_grad())
+    result.needs_broadcasting = self.needs_broadcasting
+    result.backward_fn = self.operation.backward
+    self.add_edges(result)
+    return result
+
+
+class Add(Operation):
+  def __init__(self, tens1, tens2):
+    super().__init__(self, True, tens1, tens2)
+
+  def forward(self):
+    tens1, tens2 = self.get_tensors()
+    return self.get_result_tensor(tens1.data+tens2.data)
+
+  def backward(self):
+    broadcast_shape = self.get_broadcast_shape()
+    tens1, tens2 = self.get_tensors()
+    tens1.set_grad_fn(lambda ug:np.dot(np.eye(mul_shape_dims(broadcast_shape)), ug))
+    tens2.set_grad_fn(lambda ug:np.dot(np.eye(mul_shape_dims(broadcast_shape)), ug))
+
+a = Tensor(1, requires_grad=True)
+b = Tensor([1,1,1], requires_grad=True)
+c = a+b
+c.backward([1,1,1])
+print(a.grad)
+print(b.grad)
