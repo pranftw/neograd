Finally, softmax backward added - softmax backward was incorrect, added gradients according to https://towardsdatascience.com/derivative-of-the-softmax-function-and-the-categorical-cross-entropy-loss-ffceefc081d1 - tensor type will be float - conv nets will be tested next, finally

neograd/autograd/ops.py
@@ -235,7 +235,7 @@ class Sum(Operation):
         lg = np.ones(tens.shape)
 
       if self.axis is not None:
-        grads = np.dot(lg, ug)
+        grads = lg*ug
         try:
           num_repeat = tens.shape[self.axis]
         except IndexError:
@@ -319,7 +319,18 @@ class Softmax(Operation):
     return self.get_result_tensor(self.calc_softmax(tens), tens)
   
   def backward(self, tens):
-    tens.set_grad_fn(lambda ug:0*ug)
+    def softmax_backward(arr): # arr will always be 1d array
+      grads = -np.broadcast_to(arr, (arr.size, arr.size))
+      np.fill_diagonal(grads, 1+(np.diagonal(grads)))
+      grads *= arr
+      return np.dot(grads, arr)
+
+    def grad_backward(ug):
+      result = self.calc_softmax(tens)
+      local_grads = np.apply_along_axis(softmax_backward, self.axis, result)
+      return local_grads*ug
+
+    tens.set_grad_fn(grad_backward)
 
   def calc_softmax(self, tens):
     max_vals = np.amax(tens.data, axis=self.axis)

neograd/autograd/utils.py
@@ -15,7 +15,7 @@ def process_data(data):
     if not isinstance(data, np.ndarray):
       data = np.array(data)
     try:
-      data = data.astype(np.float32)
+      data = data.astype(float)
     except ValueError:
       raise TypeError("Elements of data should be of type float or be typecastable to float")
   else:

tests/test.py
@@ -1,51 +1,56 @@
 import _setup
 import neograd as ng
 import numpy as np
-from neograd.nn.loss import BCE
-from neograd.nn.optim import Adam
-from sklearn.datasets import make_circles
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report, accuracy_score
+# from neograd.nn.loss import BCE
+# from neograd.nn.optim import Adam
+# from sklearn.datasets import make_circles
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import classification_report, accuracy_score
 
-X, y = make_circles(n_samples=1000, noise=0.05, random_state=100)
-X_train, X_test, y_train, y_test = train_test_split(X,y)
+# X, y = make_circles(n_samples=1000, noise=0.05, random_state=100)
+# X_train, X_test, y_train, y_test = train_test_split(X,y)
 
-X_train, X_test = ng.tensor(X_train), ng.tensor(X_test)
-y_train, y_test = ng.tensor(y_train.reshape(750,1)), ng.tensor(y_test.reshape(250,1))
+# X_train, X_test = ng.tensor(X_train), ng.tensor(X_test)
+# y_train, y_test = ng.tensor(y_train.reshape(750,1)), ng.tensor(y_test.reshape(250,1))
 
-num_train = 750
-num_test = 250
-num_iter = 50
+# num_train = 750
+# num_test = 250
+# num_iter = 50
 
-class NN(ng.nn.Model):
-  def __init__(self):
-    super().__init__(self)
-    self.stack = ng.nn.Sequential(
-      ng.nn.Linear(2,100),
-      ng.nn.ReLU(),
-      ng.nn.Linear(100,1),
-      ng.nn.Sigmoid()
-    )
+# class NN(ng.nn.Model):
+#   def __init__(self):
+#     super().__init__(self)
+#     self.stack = ng.nn.Sequential(
+#       ng.nn.Linear(2,100),
+#       ng.nn.ReLU(),
+#       ng.nn.Linear(100,1),
+#       ng.nn.Sigmoid()
+#     )
   
-  def forward(self, inputs):
-    return self.stack(inputs)
+#   def forward(self, inputs):
+#     return self.stack(inputs)
 
-model = NN()
-loss_fn = BCE()
-optim = Adam(model.get_params(), 0.05)
+# model = NN()
+# loss_fn = BCE()
+# optim = Adam(model.get_params(), 0.05)
 
-for iter in range(num_iter):
-  optim.zero_grad()
-  outputs = model(X_train)
-  loss = loss_fn(outputs, y_train)
-  loss.backward()
-  optim.step()
-  print(f"iter {iter+1}/{num_iter}\nloss: {loss}\n")
+# for iter in range(num_iter):
+#   optim.zero_grad()
+#   outputs = model(X_train)
+#   loss = loss_fn(outputs, y_train)
+#   loss.backward()
+#   optim.step()
+#   print(f"iter {iter+1}/{num_iter}\nloss: {loss}\n")
 
-with ng.no_track():
-  test_outputs = model(X_test)
-  preds = np.where(test_outputs.data>=0.5, 1, 0)
+# with ng.no_track():
+#   test_outputs = model(X_test)
+#   preds = np.where(test_outputs.data>=0.5, 1, 0)
 
-print(classification_report(y_test.data.astype(int).flatten(), preds.flatten()))
-print(accuracy_score(y_test.data.astype(int).flatten(), preds.flatten()))
+# print(classification_report(y_test.data.astype(int).flatten(), preds.flatten()))
+# print(accuracy_score(y_test.data.astype(int).flatten(), preds.flatten()))
 
+from neograd.autograd.ops import softmax
+a = ng.tensor([1,2,3], requires_grad=True)
+b = softmax(a, 0)
+b.backward([1,1,1])
+print(a.grad)