FINALLY SOFTMAX WORKS - the issue was that I was using a counter to get the upper_grads slice by slice, that counter wasnt getting incremented at all, so the gradients were incorrect, so fixed that - grad_check passes for mnist.py too woahh

neograd/autograd/ops.py
@@ -321,12 +321,11 @@ class Softmax(Operation):
     return self.get_result_tensor(result, tens)
   
   def backward(self, tens):
-    def softmax_grad(arr, ug_slices, counter): # arr will always be 1d array
+    def softmax_grad(arr, ug_slices): # arr will always be 1d array
       local_grad = -np.broadcast_to(arr, (arr.size, arr.size))
       np.fill_diagonal(local_grad, 1+np.diagonal(local_grad))
       local_grad = local_grad*arr.reshape(arr.size, 1)
-      result = np.dot(local_grad, ug_slices[counter])
-      counter+=1
+      result = np.dot(local_grad, ug_slices.pop(0))
       return result
     
     def get_ug_slices(arr, ug_slices):
@@ -334,10 +333,9 @@ class Softmax(Operation):
 
     def grad_backward(ug):
       result = np.apply_along_axis(self.calc_softmax, self.axis, tens.data)
-      counter = 0
       ug_slices = []
       np.apply_along_axis(get_ug_slices, self.axis, ug, ug_slices)
-      grads = np.apply_along_axis(softmax_grad, self.axis, result, ug_slices, counter)
+      grads = np.apply_along_axis(softmax_grad, self.axis, result, ug_slices)
       return grads
 
     tens.set_grad_fn(grad_backward)

tests/mnist.py
@@ -2,7 +2,7 @@ import _setup
 import neograd as ng
 import numpy as np
 from neograd.nn.loss import CE
-from neograd.nn.optim import GD
+from neograd.nn.optim import Adam
 from neograd.nn.utils import grad_check
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
@@ -11,8 +11,8 @@ from sklearn.metrics import classification_report, accuracy_score
 X, y = load_digits(return_X_y=True)
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-num_train, num_test = 20, 10
-num_iter = 10
+num_train, num_test = 10, 10
+num_iter = 1000
 
 def one_hot(cls_arr, num_examples, num_classes):
   encoded = np.zeros((num_examples, num_classes))
@@ -41,9 +41,14 @@ class NN(ng.nn.Model):
   def __init__(self):
     super().__init__(self)
     self.stack = ng.nn.Sequential(
-      ng.nn.Linear(64,10),
+      ng.nn.Linear(64,100),
       ng.nn.ReLU(),
-      ng.nn.Softmax(axis=1)
+      ng.nn.Linear(100,50),
+      ng.nn.Tanh(),
+      ng.nn.Linear(50,25),
+      ng.nn.ReLU(),
+      ng.nn.Linear(25,10),
+      ng.nn.Softmax(1)
     )
   
   def forward(self, inputs):
@@ -52,7 +57,7 @@ class NN(ng.nn.Model):
 model = NN()
 
 loss_fn = CE()
-optim = GD(model.get_params(), 5e-5)
+optim = Adam(model.get_params(), 5e-3)
 
 # for iter in range(num_iter):
 #   optim.zero_grad()
@@ -60,8 +65,8 @@ optim = GD(model.get_params(), 5e-5)
 #   loss = loss_fn(outputs, y_train)
 #   loss.backward()
 #   optim.step()
-#   print(f"iter {iter+1}/{num_iter}\nloss: {loss}\n")
-  # if iter%50==0:
-  #   print(f"iter {iter+1}/{num_iter}\nloss: {loss}\n")
+#   # print(f"iter {iter+1}/{num_iter}\nloss: {loss}\n")
+#   if iter%50==0:
+#     print(f"iter {iter+1}/{num_iter}\nloss: {loss}\n")
 
 grad_check(model, X_train, y_train, loss_fn)