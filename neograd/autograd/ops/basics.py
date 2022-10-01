ops tests run without pytorch + sum backward changes - to test ops we now use fn_grad_check - sum backward greatly simplified, and fixed an error it was giving on gradient check on axis=1 and made it more generic - in fn_grad_check now inputs and inputs and params are now tuples, also any kwargs can be passed to fn thats being checked - both grad_check and fn_grad_check have print_vals as a arg to turn off or on - in wiggle_params, params grad after wiggling is zero_grad ed to prevent some unusual vals - in loss.py if shape of outputs is (), then num_examples are taken as 1 and get_num_examples added - completely revamped test_ops.py - with this change, new tests can be added easily without dependency on pytorch

neograd/autograd/ops/basics.py
@@ -132,29 +132,12 @@ class Sum(Operation):
     return self.get_result_tensor(np.sum(tens.data, axis=self.axis), tens)
   
   def backward(self, tens):
-    def grad_backward(ug):
-      tens_shape = list(tens.shape)
+    def sum_backward(ug):
       if self.axis is not None:
-        try:
-          tens_shape[self.axis] = 1
-        except IndexError:
-          pass
-        lg = 1
-      else:
-        lg = np.ones(tens.shape)
-
-      if self.axis is not None:
-        grads = np.dot(lg,ug)
-        try:
-          num_repeat = tens.shape[self.axis]
-        except IndexError:
-          num_repeat = 1
-        grads = grads[np.newaxis]
-        grads = np.concatenate([grads]*num_repeat)
-      else:
-        grads = lg*ug
+        ug = np.expand_dims(ug, axis=self.axis)
+      grads = np.ones(tens.shape)*ug
       return grads
-    tens.set_grad_fn(grad_backward)
+    tens.set_grad_fn(sum_backward)
 
 def sum(tens, axis=None):
   return Sum(axis).forward(tens)

neograd/autograd/utils.py
@@ -93,13 +93,15 @@ class no_track:
     self.graph.track = True
 
 
-def _evaluate_grad_check(analytical_grads, calculated_grads, epsilon):
+def _evaluate_grad_check(analytical_grads, calculated_grads, epsilon, print_vals):
   dist = np.linalg.norm(analytical_grads-calculated_grads)/(np.linalg.norm(analytical_grads) + np.linalg.norm(calculated_grads))
-  print("Gradient Check Distance:", dist)
-  if dist<epsilon:
-    print("Gradient Check PASSED")
-  else:
-    print("Gradient Check FAILED")
+  if print_vals:
+    print("Gradient Check Distance:", dist)
+    if dist<epsilon:
+      print("Gradient Check PASSED")
+    else:
+      print("Gradient Check FAILED")
+  return dist
 
 
 def _wiggle_params(analytical_grads, calculated_grads, params, get_loss, epsilon):
@@ -116,9 +118,10 @@ def _wiggle_params(analytical_grads, calculated_grads, params, get_loss, epsilon
           param.data[idx]+=epsilon # ORIGINAL
         calculated_grads.append(param.grad[idx])
         analytical_grads.append((loss1.data-loss2.data)/(2*epsilon))
+    param.zero_grad()
 
 
-def grad_check(model, inputs, targets, loss_fn, epsilon=1e-7):
+def grad_check(model, inputs, targets, loss_fn, epsilon=1e-7, print_vals=True):
   '''
     Implements Gradient Check, to make sure that backprop is calculating
       the right gradients.
@@ -149,10 +152,10 @@ def grad_check(model, inputs, targets, loss_fn, epsilon=1e-7):
 
   analytical_grads = np.array(analytical_grads)
   calculated_grads = np.array(calculated_grads)
-  _evaluate_grad_check(analytical_grads, calculated_grads, epsilon)
+  return _evaluate_grad_check(analytical_grads, calculated_grads, epsilon, print_vals)
 
 
-def fn_grad_check(fn, inputs, *params, targets=None, loss_fn=None, epsilon=1e-7):
+def fn_grad_check(fn, inputs, params, targets=None, loss_fn=None, epsilon=1e-7, print_vals=True, **kwargs):
   '''
     Implements Gradient Check for a function instead of a complete model
     Any params that are required to be gradient checked can be specified
@@ -160,8 +163,8 @@ def fn_grad_check(fn, inputs, *params, targets=None, loss_fn=None, epsilon=1e-7)
 
     Params:
       fn - Function to be gradient checked
-      inputs:Tensor - inputs to the function
-      params:*(Tensor) - the params whose data can be wiggled to get the gradients
+      inputs:tuple(Tensor) - inputs to the function
+      params:tuple(Tensor) - the params whose data can be wiggled to get the gradients
       targets:Tensor - targets of the function
       loss_fn:Loss - loss_fn to evaluate the function
       epsilon:float
@@ -173,7 +176,7 @@ def fn_grad_check(fn, inputs, *params, targets=None, loss_fn=None, epsilon=1e-7)
   calculated_grads = []
 
   def get_loss(targets=targets):
-    outputs = fn(inputs)
+    outputs = fn(*inputs, **kwargs)
     if targets is None:
       from .tensor import Tensor as tensor
       targets = tensor(np.ones(outputs.shape))
@@ -187,4 +190,4 @@ def fn_grad_check(fn, inputs, *params, targets=None, loss_fn=None, epsilon=1e-7)
 
   analytical_grads = np.array(analytical_grads)
   calculated_grads = np.array(calculated_grads)
-  _evaluate_grad_check(analytical_grads, calculated_grads, epsilon)
+  return _evaluate_grad_check(analytical_grads, calculated_grads, epsilon, print_vals)

tests/test_ops.py
@@ -1,88 +1,33 @@
 import _setup
 import numpy as np
-import torch
 import neograd as ng
-from neograd.autograd.ops import relu as ng_relu, sigmoid as ng_sigmoid, tanh as ng_tanh
-from neograd.autograd.utils import process_data
+from neograd.autograd.ops import relu, sigmoid, tanh
+from neograd.autograd.utils import process_data, fn_grad_check
 
 
-def to_tensors(tens_cls, operands):
+def to_tensors(operands):
   '''
-    Converts all operands of type numpy to tens_cls which can be torch.tensor or ng.tensor
+    Converts all operands of type numpy to ng.tensor
 
     Params:
-      tens_cls:torch.tensor/ng.tensor - The Tensor type to which the operands should be converted to
       operands:(np.ndarray)
     
     Returns:
-      tensors:[Tensor]
+      tensors:[ng.tensor]
   '''
   tensors = []
   for operand in operands:
-    operand = operand.astype(float) # Torch only accepts floating point data for requires_grad=True
-    tensors.append(tens_cls(operand, requires_grad=True))
-  return tensors
+    operand = operand.astype(float)
+    tensors.append(ng.tensor(operand, requires_grad=True))
+  return tuple(tensors)
 
-def execute_neograd(fn, *operands, **kwargs):
+def execute(fn, *operands, epsilon=1e-7, **kwargs):
   '''
-    Executes the given function on the operands in neograd
-
-    Params:
-      fn:ng_operation
-      operands:*args(np.ndarray)
-      **kwargs - any keyword args to fn
-    
-    Returns:
-      rounded_result:np.ndarray - Result rounded to 2 places
-      grads:[np.ndarray] - Each array rounded to 2 places
+    Performs the grad_check on the fn using fn_grad_check
   '''
-  tensors = to_tensors(ng.tensor, operands)
-  result = fn(*tensors, **kwargs)
-  result.backward(np.ones(result.shape).astype(float))
-  result_np = process_data(result.data)
-  rounded_result = np.around(result_np, decimals=2)
-  grads = []
-  for tens in tensors:
-    grads.append(np.around(tens.grad, decimals=2))
-  return rounded_result, grads
-
-def execute_torch(fn, *operands, **kwargs):
-  '''
-    Executes the given function on the operands in torch
-
-    Params:
-      fn:torch_operation
-      operands:*args(np.ndarray)
-      **kwargs - any keyword args to fn
-    
-    Returns:
-      rounded_result:np.ndarray - Result rounded to 2 places
-      grads:[np.ndarray] - Each array rounded to 2 places
-  '''
-  tensors = to_tensors(torch.tensor, operands)
-  result = fn(*tensors, **kwargs)
-  result.backward(torch.tensor(np.ones(result.shape).astype(float)))
-  result_torch = process_data(result.detach().numpy())
-  rounded_result = np.around(result_torch, decimals=2)
-  grads = []
-  for tens in tensors:
-    grads.append(np.around(tens.grad.numpy(), decimals=2))
-  return rounded_result, grads
-
-def compare_outputs(ng_output, torch_output):
-  '''
-    Compares the outputs after execute_neograd and execute_torch
-    Checks equality of respective results
-    Checks equality of grads length
-    Checks the equality of each grad in grads
-  '''
-  result_ng, grads_ng = ng_output
-  result_torch, grads_torch = torch_output
-  assert np.array_equal(result_ng, result_torch)
-  assert len(grads_ng)==len(grads_torch)
-  for grad_ng, grad_torch in zip(grads_ng, grads_torch):
-    assert np.array_equal(grad_ng, grad_torch)
-
+  tensors = to_tensors(operands)
+  dist = fn_grad_check(fn, tensors, tensors, epsilon=epsilon, print_vals=False, **kwargs)
+  assert dist<epsilon
 
 a = np.array(3)
 b = np.array([1,2,3])
@@ -94,69 +39,69 @@ f = np.array([[0.5, -2, 1], [-1, -0.4, 20]])
 
 # <------------ADD------------>
 def test_add():
-  compare_outputs(execute_neograd(ng.add, a, b), execute_torch(torch.add, a, b))
-  compare_outputs(execute_neograd(ng.add, a, d), execute_torch(torch.add, a, d))
+  execute(ng.add, a, b)
+  execute(ng.add, a, d)
 
 
 # <------------SUB------------>
 def test_sub():
-  compare_outputs(execute_neograd(ng.sub, a, b), execute_torch(torch.sub, a, b))
-  compare_outputs(execute_neograd(ng.sub, a, d), execute_torch(torch.sub, a, d))
+  execute(ng.sub, a, b)
+  execute(ng.sub, a, d)
 
 
 # <------------MUL------------>
 def test_mul():
-  compare_outputs(execute_neograd(ng.mul, b, c), execute_torch(torch.mul, b, c))
-  compare_outputs(execute_neograd(ng.mul, c, d), execute_torch(torch.mul, c, d))
+  execute(ng.mul, b, c)
+  execute(ng.mul, c, d)
 
 
 # <------------DIV------------>
 def test_div():
-  compare_outputs(execute_neograd(ng.div, b, c), execute_torch(torch.div, b, c))
+  execute(ng.div, b, c)
 
 
 # <------------DOT------------>
 def test_dot():
-  compare_outputs(execute_neograd(ng.dot, e, c), execute_torch(torch.mm, e, c))
+  execute(ng.dot, e, c)
 
 
 # <------------EXP------------>
 def test_exp():
-  compare_outputs(execute_neograd(ng.exp, e), execute_torch(torch.exp, e))
+  execute(ng.exp, e)
 
 
 # <------------LOG------------>
 def test_log():
-  compare_outputs(execute_neograd(ng.log, d), execute_torch(torch.log, d))
+  execute(ng.log, d)
 
 
 # <------------POW------------>
 def test_pow():
-  compare_outputs(execute_neograd(ng.pow, c, b), execute_torch(torch.pow, c, b))
+  execute(ng.pow, c, b)
 
 
 # <------------SUM------------>
 def test_sum():
-  compare_outputs(execute_neograd(ng.sum, d), execute_torch(torch.sum, d))
-  compare_outputs(execute_neograd(ng.sum, d, axis=0), execute_torch(torch.sum, d, dim=0))
-  compare_outputs(execute_neograd(ng.sum, d, axis=1), execute_torch(torch.sum, d, dim=1))
+  execute(ng.sum, d)
+  execute(ng.sum, d, axis=0)
+  execute(ng.sum, d, axis=1)
 
 
 # <------------TRANSPOSE------------>
 def test_transpose():
-  compare_outputs(execute_neograd(ng.transpose, c), execute_torch(torch.transpose, c, dim0=0, dim1=1))
+  execute(ng.transpose, c)
 
 
 # <------------RELU------------>
 def test_relu():
-  compare_outputs(execute_neograd(ng_relu, f), execute_torch(torch.relu, f))
+  execute(relu, f)
 
 
 # <------------SIGMOID------------>
 def test_sigmoid():
-  compare_outputs(execute_neograd(ng_sigmoid, c), execute_torch(torch.sigmoid, c))
+  execute(sigmoid, c)
 
 
 # <------------TANH------------>
 def test_tanh():
-  compare_outputs(execute_neograd(ng_tanh, f), execute_torch(torch.tanh, f))
+  execute(tanh, f)