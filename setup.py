3ada41d125770aa4b87734def57957588c22a265
Pranav Sastry <pranava.sri@gmail.com>
2022-10-14 17:32:02 +0530

    PREPARING FOR V0.0.2 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_file(fpath):
 
 setup(
   name = 'neograd',
-  version = '0.0.1',
+  version = '0.0.2',
   author = 'Pranav Sastry',
   author_email = 'pranava.sri@gmail.com',
   maintainer = 'Pranav Sastry',
